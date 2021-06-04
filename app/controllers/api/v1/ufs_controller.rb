b6c52463b1d196774961d1f15e96581d3cfc22fe
Mauricio Mora <mauricios.mora@gmail.com>
2021-06-04 00:26:55 -0400

    Listo para heroku

--- app/controllers/api/v1/ufs_controller.rb
+++ app/controllers/api/v1/ufs_controller.rb
@@ -6,7 +6,18 @@ module Api
                 @client.log_in_count += 1
                 @client.save
                 @uf = Uf.find_by(date: params[:date])
-                render json: @uf
+                    if @uf == nil
+                        @uf = 'La fecha ingresada es incorrecta o no se encuentra en la base de datos'
+                        render json: @uf
+                    else 
+                        render json: @uf
+                    end
+                
+            end
+
+            def client
+                @client = Client.find_by(name: params[:name])
+                render json: @client
             end
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'by_date/:date', to: 'ufs#by_date'
+      get 'client/:name',  to: 'ufs#client'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
